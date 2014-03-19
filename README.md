80578ce088bc41dd6e563c31f5fca6caea0e66f5
Bojan Djurkovic <dbojan@gmail.com>
2014-03-19 11:38:08 -0300

    initial commit.

---
+++ README.md
@@ -0,0 +1,117 @@
+# nbrite
+
+Simple Node.js helper module for [Eventbrite API V3]((http://developer.eventbrite.com/docs/).
+
+Please see the [Eventbrite API documentation](http://developer.eventbrite.com/docs/) for full reference.
+Currently this is a preview release of the new API so it may be modified without notice. As of this writing,
+most endpoints are `GET` only. This library is likely to change. You probably **do not** want to use this in production.
+
+## Installation
+
+`npm install nbrite`
+
+## Usage overview
+
+This module works by providing proxy objects for interacting with  different resources through the Eventbrite API.
+
+Basic usage to get event info:
+
+```
+var Nbrite = require('nbrite');
+var nbrite = new Nbrite(EVENTBRITE_ACCESS_TOKEN);
+
+nbrite.events('123456').info(function (err, event) {
+  // `event` is parsed JSON response from the API
+  console.dir(event);
+});
+```
+
+Something more elaborate. Get event info, attendees, and attendee details. Notice that the proxy objects can be reused.
+
+```
+var event = nbrite.events('123456');
+
+event.info(function (err, data) {
+  // `data` is event info
+  console.log(data);
+});
+
+event.attendees().list(function (err, attendees) {
+  // `attendees` is the list of attendees
+  console.log(attendees);
+});
+
+event.attendees('87654321').info(function (err, data) {
+  // `data` is the attendee details
+  console.log(data);
+});
+```
+
+For full usage examples see the `/test` folder.
+
+## Generic Requests
+`nbrite` provides `get`, `post`, `put`, `patch` and `delete` functions which can make requests with the specified HTTP method to any endpoint:
+
+```
+nbrite.get('/orders', function (err, orders) {
+});
+
+nbrite.get('/events/123456/attendees', { status: 'not_attending' }, function (err, attendees) {
+});
+```
+
+## Promises
+
+Module works with Node-style callbacks, but also implements promises with the [Q](http://github.com/kriskowal/q) library.
+
+```
+nbrite.users('44499922255').info().then(function (data) {
+  console.log(data);
+}, function (err) {
+  console.log(err);
+});
+```
+
+The function passed as 2nd argument is optional and not needed if you don't care about the fail case.
+
+## Tests
+
+To run the test suite you must first create a file named `/test/auth.json`, which contains your credentials as JSON, for example:
+
+```json
+{ "access_token": "ABCD123ABCD123ABCD123A" }
+```
+
+Also create `/test/fixture.json` with some of your existing data:
+
+```json
+{
+  "existing_event_id": "1234567890",
+  "existing_event_id_teams": "2233445566",
+  "existing_user_id": "44499922255",
+  "existing_attendee_id": "222999555",
+  "existing_team_id": "123456",
+  "existing_order_id": "113331166"
+}
+```
+
+`existing_event_id_teams` should be an event with a group or team registrations.
+
+Then install the dev dependencies and execute the test suite:
+
+```
+$ npm install
+$ npm test
+```
+
+The tests will call Eventbrite API.
+
+## TODO
+* Promise tests.
+* POST functions.
+* New API resources as they are exposed.
+
+## Notes
+
+This library started out as just an experiment.
+It was heavily influenced by [node-heroku-client](https://github.com/jclem/node-heroku-client).
---
+++ test/nbrite.test.js
@@ -0,0 +1,209 @@
+var auth = require('./auth.json');
+var fixture = require('./fixture.json');
+var assert = require('assert');
+var Nbrite = require('../lib/Nbrite');
+
+var nbrite;
+
+module.exports = {
+  before: function (fn) {
+    nbrite = new Nbrite(auth.access_token);
+    fn();
+  },
+
+  /*
+   * Event functions
+   */
+
+  'get event details': function (done) {
+    nbrite.events(fixture.existing_event_id).info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+
+  'get event attendees invalid status': function (done) {
+    nbrite.events(fixture.existing_event_id).attendees().list({status: 123}, function (err, body) {
+      assert.ok(err);
+      assert(/Invalid parameter type./.test(err.message));
+      done();
+    });
+  },
+
+  'get event attendees with status': function (done) {
+    nbrite.events(fixture.existing_event_id).attendees().list({status: 'attending'}, function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.attendees);
+      done();
+    });
+  },
+
+  'get event attendees without status': function (done) {
+    nbrite.events(fixture.existing_event_id).attendees().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.attendees);
+      done();
+    });
+  },
+
+  'get event attendees details': function (done) {
+    nbrite.events(fixture.existing_event_id).attendees(fixture.existing_attendee_id).info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+
+  'get event orders': function (done) {
+    nbrite.events(fixture.existing_event_id).orders().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.orders);
+      done();
+    });
+  },
+
+  'get event discounts': function (done) {
+    nbrite.events(fixture.existing_event_id).discounts().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.discounts);
+      done();
+    });
+  },
+
+  // TODO test for discount create
+
+  'get event access codes': function (done) {
+    nbrite.events(fixture.existing_event_id).accessCodes().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.access_codes);
+      done();
+    });
+  },
+
+  'get event transfers': function (done) {
+    nbrite.events(fixture.existing_event_id).transfers().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.transfers);
+      done();
+    });
+  },
+
+  'get event teams': function (done) {
+    nbrite.events(fixture.existing_event_id_teams).teams().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.teams);
+      done();
+    });
+  },
+
+  'get event team details': function (done) {
+    nbrite.events(fixture.existing_event_id_teams).teams(fixture.existing_team_id).info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+
+  'get event team attendees': function (done) {
+    nbrite.events(fixture.existing_event_id_teams).teams(fixture.existing_team_id).attendees().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.attendees);
+      done();
+    });
+  },
+
+  /*
+   * User functions
+   */
+
+  'get user details': function (done) {
+    nbrite.users(fixture.existing_user_id).info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+
+  'get user me()': function (done) {
+    nbrite.users().me().info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+
+  'get user orders': function (done) {
+    nbrite.users(fixture.existing_user_id).orders().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.orders);
+      done();
+    });
+  },
+
+  'get user owned events': function (done) {
+    nbrite.users(fixture.existing_user_id).ownedEvents().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.events);
+      done();
+    });
+  },
+
+  'get user owned event\'s orders': function (done) {
+    nbrite.users(fixture.existing_user_id).ownedEventOrders().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.orders);
+      done();
+    });
+  },
+
+  'get user owned event\'s attendees': function (done) {
+    nbrite.users(fixture.existing_user_id).ownedEventAttendees().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.attendees);
+      done();
+    });
+  },
+
+  'get user venues': function (done) {
+    nbrite.users(fixture.existing_user_id).venues().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.venues);
+      done();
+    });
+  },
+
+  'get user organizers': function (done) {
+    nbrite.users(fixture.existing_user_id).organizers().list(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      assert.ok(body.organizers);
+      done();
+    });
+  },
+
+  /*
+   * Order functions
+   */
+
+  'get order details': function (done) {
+    nbrite.orders(fixture.existing_order_id).info(function (err, body) {
+      assert.ifError(err);
+      assert.ok(body);
+      done();
+    });
+  },
+};
